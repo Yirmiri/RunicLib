964878e61e5c4d04681122f0edbdc907f8b18db1
Artyrian <77861508+TheArtyrian@users.noreply.github.com>
2026-09-24 11:28:29 -0400

    Fixed Server iterator

--- common/src/main/java/net/azurune/runiclib/core/mixin/server/ServerLevelMixin.java
+++ common/src/main/java/net/azurune/runiclib/core/mixin/server/ServerLevelMixin.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,11 +23,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 @Mixin(ServerLevel.class)
+@Debug(export = true)
 public abstract class ServerLevelMixin extends Level implements IRunicServerLvl {
     protected ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, Supplier<ProfilerFiller> profiler, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
         super(levelData, dimension, registryAccess, dimensionTypeRegistration, profiler, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
@@ -51,7 +54,12 @@ public abstract class ServerLevelMixin extends Level implements IRunicServerLvl 
     private void runiclib$tickAndUpdateContainers(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
         // Run list
         if (!this.rl$tickableContainers.isEmpty()) {
-            for (BlockPos pos : this.rl$tickableContainers) {
+            Iterator<BlockPos> itr = this.rl$tickableContainers.iterator();
+
+            BlockPos pos;
+            while (itr.hasNext()) {
+                pos = itr.next();
+
                 BlockEntity entity = this.getBlockEntity(pos);
                 if (entity instanceof Container container && !entity.isRemoved() && shouldTickBlocksAt(pos)) {
                     if (!container.isEmpty()) {
@@ -73,11 +81,7 @@ public abstract class ServerLevelMixin extends Level implements IRunicServerLvl 
 
         // Remove positions
         if (!this.rl$tickableContainersScheduledForReset.isEmpty()) {
-            for (BlockPos pos : this.rl$tickableContainersScheduledForReset) {
-                this.rl$tickableContainers.remove(pos);
-                RunicLib.LOGGER.debug("Removed container at {} from Runic's container ticker", pos.toString());
-            }
-
+            this.rl$tickableContainers.removeAll(this.rl$tickableContainersScheduledForReset);
             this.rl$tickableContainersScheduledForReset.clear();
         }
     }
